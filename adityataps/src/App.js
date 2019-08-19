40a0e398d8de4bda9cd08d9b675a66fe47892ece
Aditya Tapshalkar <aditya.taps@gatech.edu>
2019-08-19 12:18:01 -0400

    Completion of Frontpage

--- adityataps/src/App.js
+++ adityataps/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './APictureOfMe.jpeg';
 import name from './ThisIsMyName.png';
-
-import outlook from './social/microsoftoutlook.svg';
-// import { ReactComponent as outlook } from './social/microsoftoutlook.svg'
-import fb from './social/facebook.svg';
-import github from './social/github.svg';
-import linkedin from './social/linkedin.svg';
+import {
+    BrowserView,
+    MobileView,
+    isBrowser,
+    isMobile
+} from "react-device-detect";
 
 import './App.css';
 
@@ -15,34 +15,54 @@ function App() {
   return (
     <div className="Home">
       <header className="App-header">
-      <br></br> 
-{/*        <img src={logo} className="APictureOfMe" alt="logo" height="400" width="400"/> */}
+      <br></br>
         <br></br>
         <img src={name} className="ThisIsMyName" alt="name" height="200" width="800"/>
         <p>
-          
+
+            <BrowserView>
+          <div class="options-container">
           <a className="App-link" 
           class="animated fadeIn delay-1" href="http://www.google.com">about</a> <b class="animated fadeIn delay-15"> // </b> <a className="App-link" 
           class="animated fadeIn delay-2" href="http://www.google.com">projects</a> <b class="animated fadeIn delay-25"> // </b> <a className="App-link" 
           class="animated fadeIn delay-3" href="http://www.google.com">resumé</a> <b class="animated fadeIn delay-35"> // </b> <a className="App-link" 
           class="animated fadeIn delay-4" href="http://www.google.com">contact</a>
-        
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
+
+          </div>
+            </BrowserView>
+
+            <MobileView>
+                <br></br>
+                <br></br>
+                <div class="mobile-options">
+                    <a className="App-link"
+                       className="animated fadeIn delay-1" href="http://www.google.com">about</a><br></br>
+                    <a className="App-link"
+                       className="animated fadeIn delay-2" href="http://www.google.com">projects</a><br></br>
+                    <a className="App-link"
+                       className="animated fadeIn delay-3" href="http://www.google.com">resumé</a><br></br>
+                    <a className="App-link"
+                       className="animated fadeIn delay-4" href="http://www.google.com">contact</a><br></br>
+                </div>
+            </MobileView>
+
         <br></br>
         <br></br>
         <br></br>
 
         <div class="icons-container animated fadeIn delay-5">
-          <span class="iconify" data-icon="entypo-social:facebook-with-circle" data-inline="false"></span>
-          <span class="iconify" data-icon="entypo-social:linkedin-with-circle" data-inline="false"></span>        
-          <span class="iconify" data-icon="entypo-social:github-with-circle" data-inline="false"></span>
-        </div>
 
+            <a href="https://www.facebook.com/adits707" rel="noopener noreferrer" target="_blank">
+            <icon span className="iconify" data-icon="entypo-social:facebook-with-circle" data-inline="false"></icon>
+            </a>
+            <a href="https://www.linkedin.com/in/adityatapshalkar/" rel="noopener noreferrer" target="_blank">
+          <icon span class="iconify" data-icon="entypo-social:linkedin-with-circle" data-inline="false"></icon>
+            </a>
+            <a href="https://github.com/adityataps" rel="noopener noreferrer" target="_blank">
+          <icon span class="iconify" data-icon="entypo-social:github-with-circle" data-inline="false"></icon>
+            </a>
+
+            </div>
 
         </p>
       </header>
